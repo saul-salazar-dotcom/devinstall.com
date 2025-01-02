2d6f759d46fa1ea6775d6159274e37330ee2c943
Saul Salazar <saulmmendoza+github@gmail.com>
2025-01-02 13:55:17 -0600

    fix(linux): install vscode dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -54,3 +54,22 @@ install_process() {
 }
 
 install_process curl
+
+# The following code fixes the error "package code not found"
+
+# Read OS information
+. /etc/os-release
+
+# Determine the package manager and install VS Code dependencies accordingly
+if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+    sudo apt-get install wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    rm -f packages.microsoft.gpg
+    sudo apt update
+elif [[ "$ID" == "fedora" || "$ID" == "centos" || "$ID" == "rhel" ]]; then
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+    dnf check-update
+fi
